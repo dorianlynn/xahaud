96c7fe1f3e366a2eef9261d7f56b7889b1385daf
Wietse Wind <mail@wietse.com>
2023-01-31 10:32:11 +0100

    Fix auto clean

--- build-full.sh
+++ build-full.sh
@@ -56,8 +56,6 @@ then
     #endif/g" src/ripple/net/impl/RegisterSSLCerts.cpp &&
     sed -i "s/#include <ripple\/net\/RegisterSSLCerts.h>/\0\n#include <certs\/certbundle.h>/g" src/ripple/net/impl/RegisterSSLCerts.cpp
 fi
-rm -rf release-build;
-mkdir release-build;
 mkdir .nih_c;
 mkdir .nih_toolchain;
 cd .nih_toolchain &&
@@ -135,7 +133,22 @@ cmake .. \
     -DBoost_NO_BOOST_CMAKE=ON -DLLVM_DIR=/usr/lib64/llvm13/lib/cmake/llvm/ -DLLVM_LIBRARY_DIR=/usr/lib64/llvm13/lib/ &&
 make -j$3 install &&
 export PATH=`echo $PATH | sed -E "s/devtoolset-9/devtoolset-10/g"` &&
-cp -r include/api/wasmedge /usr/include/
+cp -r include/api/wasmedge /usr/include/ &&
+cd /io/ &&
+echo "-- Build Rippled --" &&
+pwd &&
+cp Builds/CMake/deps/Rocksdb.cmake Builds/CMake/deps/Rocksdb.cmake.old &&
+perl -i -pe "s/^(\\s*)-DBUILD_SHARED_LIBS=OFF/\\1-DBUILD_SHARED_LIBS=OFF\\n\\1-DROCKSDB_BUILD_SHARED=OFF/g" Builds/CMake/deps/Rocksdb.cmake &&
+mv Builds/CMake/deps/WasmEdge.cmake Builds/CMake/deps/WasmEdge.old &&
+echo "find_package(LLVM REQUIRED CONFIG)
+message(STATUS \"Found LLVM ${LLVM_PACKAGE_VERSION}\")
+message(STATUS \"Using LLVMConfig.cmake in: \${LLVM_DIR}\")
+add_library (wasmedge STATIC IMPORTED GLOBAL)
+set_target_properties(wasmedge PROPERTIES IMPORTED_LOCATION \${WasmEdge_LIB})
+target_link_libraries (ripple_libs INTERFACE wasmedge)
+add_library (NIH::WasmEdge ALIAS wasmedge)
+message(\"WasmEdge DONE\")
+" > Builds/CMake/deps/WasmEdge.cmake &&
 
 echo "MOVING TO [ build-core.sh ]"
 ./build-core.sh "$1" "$2" "$3"
